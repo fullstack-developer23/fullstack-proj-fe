f2909638615eb621e40bf872397bca977e30368d
NoraSaffar <norasaffar8910@gmail.com>
2024-01-24 10:23:36 +0000

    task: completed basic nav in header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import './App.css';
-import CheckoutPage from './pages/checkoutPage/CheckoutPage';
-import HomePage from './pages/homePage/HomePage';
+import "./App.css";
+import CheckoutPage from "./pages/checkoutPage/CheckoutPage";
+import HomePage from "./pages/homePage/HomePage";
 import UserAccountPage from "./pages/userAccountPage/UserAccountPage";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/checkout" element={<CheckoutPage />} />
-    <Route path="/youruseraccount" element={<UserAccountPage />} />
-  </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/youruseraccount" element={<UserAccountPage />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,30 @@
-import React from 'react'
-
-import ""
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header-wrapper">
+      <div className="header-inner-container">
+        <div className="logo"></div>
+        <div className="header-nav">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/checkout">Checkout</Link>
+              </li>
+              <li>
+                <Link to="/youruseraccount">User Account</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="header-message"></div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
