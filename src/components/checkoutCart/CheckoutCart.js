1523182a4f4378813547b568a7e810a6372f7e98
Xavier Pashley <xavipashley@gmail.com>
2024-01-31 16:32:16 +0000

    pay or play func nearly finished

--- src/components/checkoutCart/CheckoutCart.js
+++ src/components/checkoutCart/CheckoutCart.js
@@ -10,7 +10,7 @@ const CheckoutCart = ({ cart, quantity, games, i }) => {
           {cart.length === 5 ? (
             <p>You have a full cart</p>
           ) : (
-            <p>{cart.length} GAMES ADDED | You still have space for more</p>
+            <p>GAMES ADDED: {cart.length} | You still have space for more</p>
           )}
         </div>
         <div>
